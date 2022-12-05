11cc6044355d4aa562716ef62c0652a8703c2bbf
Nay Ferreira <developer.nayferreira@gmail.com>
2022-12-04 21:56:06 -0300

    Métodos CRUD para para o endpoint de Restaurantes

--- cadastro/src/main/java/com/github/nfers/ifood/cadastro/RestauranteResource.java
+++ cadastro/src/main/java/com/github/nfers/ifood/cadastro/RestauranteResource.java
@@ -1,13 +1,20 @@
 package com.github.nfers.ifood.cadastro;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
+import javax.transaction.Transactional;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 
 @Path("/restaurantes")
@@ -16,7 +23,45 @@ import javax.ws.rs.core.MediaType;
 public class RestauranteResource {
 
     @GET
-    public List<Restaurante> hello() {
+    public List<Restaurante> buscarTodos() {
         return Restaurante.listAll();
+    }
+    
+    @POST
+    @Transactional
+    public void criar(Restaurante dto) {
+    	dto.persist();
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void atualizar(@PathParam("id") Long id, Restaurante dto ) {
+    	
+    	Optional<PanacheEntityBase> restauranteOp = Restaurante.findByIdOptional(id);
+    	
+    	if(restauranteOp.isEmpty()) {
+    		throw new NotFoundException();
+    	}
+    	
+    	Restaurante restaurante = (Restaurante) restauranteOp.get();
+    	
+    	restaurante.nome = dto.nome;
+    	dto.persist();
+    }
+    
+    
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void deletar(@PathParam("id") Long id, Restaurante dto ) {
+    	
+    	Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+    	
+    	restauranteOp.ifPresentOrElse(Restaurante::delete, () -> {
+    		throw new NotFoundException();
+    	}
+    	);
+    	
     }
 }
--- cadastro/src/test/java/com/github/nfers/ifood/cadastro/HelloResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.github.nfers.ifood.cadastro;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HelloResourceIT extends HelloResourceTest {
-    // Execute the same tests but in packaged mode.
-}
