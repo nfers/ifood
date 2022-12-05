c52a4a8c6d370e03d2a3d317cbee844f85b52e46
Nayara Ferreira <ncy.fers@gmail.com>
2022-12-05 20:41:04 -0300

    criacao do primeiro caso de teste

--- cadastro/src/test/java/com/github/nfers/ifood/cadastro/RestauranteResourceTest.java
+++ cadastro/src/test/java/com/github/nfers/ifood/cadastro/RestauranteResourceTest.java
@@ -1,17 +1,21 @@
 package com.github.nfers.ifood.cadastro;
 
-import org.approvaltests.Approvals;
-import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
 
 import com.github.database.rider.cdi.api.DBRider;
+import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.core.api.configuration.DBUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 
-import static io.restassured.RestAssured.given;
+
 
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
@@ -21,12 +25,12 @@ public class RestauranteResourceTest {
 
 	@Test
 	@DataSet("restaurantes-cenario-1.yml")
-	private void testBuscarRestaurantes() {
+	private void testCriar() {
 		String resultado = given().when().get("/restaurantes").then().statusCode(200).extract().asString();
 
 		Approvals.verifyJson(resultado);
-
 	}
+
 
 
 }
